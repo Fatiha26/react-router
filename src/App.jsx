d40c2f211bdbebe8ebc4cb21347531ae3ee83ad7
Lamia Fatiha <f6lamia@gmail.com>
2024-06-20 12:17:48 +0600

    navbar with router dom done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import About from './About';
 import Contact from './Contact';
 import Error from './Error';
+import Navbar from './Navbar';
+import './index.css';
 
 const App = () => {
+
     return(
         <>
+        <Navbar />
         <Routes>
             <Route exact path="/about" Component={About}/>
             <Route path="/contact" Component={Contact}/>
---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+const Navbar = () => {
+    return(
+        <>
+        <NavLink exact activeClassName="active_class" to='/about'>About</NavLink>
+        <NavLink exact activeClassName="active_class" to='/contact'>Contact</NavLink>    
+        </>
+    )
+}
+
+export default Navbar;
